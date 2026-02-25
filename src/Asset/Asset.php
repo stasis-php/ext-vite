3dc0c4fb2f93d52811e51784e301b3bf3b74858f
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2026-02-25 22:32:39 +0100

    add asset provider

---
+++ src/Asset/Asset.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stasis\Extension\Vite\Asset;
+
+final readonly class Asset
+{
+    public function __construct(
+        public string $sourcePath,
+        public string $publicPath,
+        public string $extension,
+        public ?string $mime = null,
+    ) {}
+
+    public function isScript(): bool
+    {
+        return $this->extension === 'js';
+    }
+
+    public function isFont(): bool
+    {
+        return in_array($this->extension, ['ttf', 'otf', 'woff', 'woff2'], true);
+    }
+}
---
+++ src/Asset/AssetProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stasis\Extension\Vite\Asset;
+
+class AssetProvider
+{
+    public function fromSourcePath(string $sourcePath, string $base = '/'): Asset
+    {
+        $publicPath = $this->getPublicPath($base, $sourcePath);
+        $extension = $this->getExtension($sourcePath);
+        $mime = $this->getMime($extension);
+
+        return new Asset($sourcePath, $publicPath, $extension, $mime);
+    }
+
+    private function getPublicPath(string $base, string $path): string
+    {
+        $fullPath = sprintf('%s/%s', rtrim($base, '/'), ltrim($path, '/'));
+        return str_replace(array('"', '\''), ['%22', '%27'], $fullPath);
+    }
+
+    private function getExtension(string $path): string
+    {
+        preg_match('/\.(\w+)$/u', $path, $matches);
+        return $matches[1] ?? '';
+    }
+
+    private function getMime(string $extension): ?string
+    {
+        return match ($extension) {
+            'js' => 'text/javascript',
+            'css' => 'text/css',
+            'ttf' => 'font/ttf',
+            'otf' => 'font/otf',
+            'woff' => 'font/woff',
+            'woff2' => 'font/woff2',
+            'png' => 'image/png',
+            'jpg', 'jpeg' => 'image/jpeg',
+            'gif' => 'image/gif',
+            'svg' => 'image/svg+xml',
+            'ico' => 'image/vnd.microsoft.ico',
+            default => null,
+        };
+    }
+}
