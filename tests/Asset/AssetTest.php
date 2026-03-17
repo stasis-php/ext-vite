818d76048f8af1ec04be77389aaf870ea6faf442
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2026-03-17 22:48:13 +0100

    add test for asset

---
+++ tests/Asset/AssetTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Stasis\Extension\Vite\Tests\Asset;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use Stasis\Extension\Vite\Asset\Asset;
+use PHPUnit\Framework\TestCase;
+
+class AssetTest extends TestCase
+{
+    #[DataProvider('scriptExtensionProvider')]
+    public function testIsScript(string $extension, bool $expected): void
+    {
+        $asset = new Asset("/src/path.{$extension}", "/dist/path.{$extension}", $extension);
+        $actual = $asset->isScript();
+        self::assertEquals($expected, $actual);
+    }
+
+    public static function scriptExtensionProvider(): array
+    {
+        return [
+            ['js', true],
+            ['css', false],
+        ];
+    }
+
+    #[DataProvider('fontExtensionProvider')]
+    public function testIsFontReturnsTrueForFontExtensions(string $extension, bool $expected): void
+    {
+        $asset = new Asset("/src/path.{$extension}", "/dist/path.{$extension}", $extension);
+        $actual = $asset->isFont();
+        self::assertEquals($expected, $actual);
+    }
+
+    public static function fontExtensionProvider(): array
+    {
+        return [
+            'ttf' => ['ttf', true],
+            'otf' => ['otf', true],
+            'woff' => ['woff', true],
+            'woff2' => ['woff2', true],
+            'png' => ['png', false],
+        ];
+    }
+}
